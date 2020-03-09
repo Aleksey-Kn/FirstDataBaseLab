9eb347f00be286851fe0c1ae74ca426df585f65c
Aleksey-Kn <aleksey.k2000@mail.ru>
2020-03-09 19:22:32 +0700

    Realised functional panel without add function

--- src/HorisontalPanel.java
+++ src/HorisontalPanel.java
@@ -10,28 +10,63 @@ public class HorisontalPanel extends JPanel {
         setBounds(x, y, 400, 20);
         setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
 
+        JTextField textField = new JTextField();
+        textField.addActionListener(l -> {
+            try {
+                int var = Integer.parseInt(textField.getText());
+                if (var > 0) {
+                    if (var < table.getRowCount()) {
+                        table.setRowSelectionInterval(var - 1, var - 1);
+                    } else {
+                        int n = table.getRowCount() - 1;
+                        table.setRowSelectionInterval(n, n);
+                    }
+                }
+            }catch (NumberFormatException e){
+                textField.setText("");
+            }
+        });
+
         JButton first = new JButton(new ImageIcon("first.jpeg"));
-        first.addActionListener(l -> table.setRowSelectionInterval(0, 0));
+        first.addActionListener(l -> {
+            table.setRowSelectionInterval(0, 0);
+            textField.setText("1");
+        });
         add(first);
 
         JButton previous = new JButton(new ImageIcon("prev.png"));
         previous.addActionListener(l -> {
             int index = table.getSelectedRow() - 1;
-            table.setRowSelectionInterval(index, index);
+            if(index >= 0) {
+                table.setRowSelectionInterval(index, index);
+                textField.setText(Integer.toString(index + 1));
+            }
         });
         add(previous);
         add(new JSeparator(JSeparator.VERTICAL));
+
+        add(textField);
+
+        JLabel label = new JLabel("из " + table.getRowCount());
+        add(label);
 
         add(new JSeparator(JSeparator.VERTICAL));
         JButton next = new JButton(new ImageIcon("next.png"));
         next.addActionListener(l -> {
             int index = table.getSelectedRow() + 1;
-            table.setRowSelectionInterval(index, index);
+            if(index < table.getRowCount()) {
+                table.setRowSelectionInterval(index, index);
+                textField.setText(Integer.toString(index + 1));
+            }
         });
         add(next);
 
         JButton last = new JButton(new ImageIcon("last.jpeg"));
-        last.addActionListener(l -> table.setRowSelectionInterval(table.getRowCount(), table.getRowCount()));
+        last.addActionListener(l -> {
+            int grc = table.getRowCount();
+            table.setRowSelectionInterval(grc, grc);
+            textField.setText(Integer.toString(grc));
+        });
         add(last);
         add(new JSeparator(JSeparator.VERTICAL));
 
@@ -43,6 +78,7 @@ public class HorisontalPanel extends JPanel {
             int index = table.getSelectedRow();
             String target = (String)tableModel.getValueAt(index, 0);
             tableModel.removeRow(index);
+            label.setText("из " + table.getRowCount());
             try {
                 Connection connection = Connector.getConnection();
                 Statement statement = connection.createStatement();
